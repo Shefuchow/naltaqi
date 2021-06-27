ed3897d38fbca3310d0d13a976faaacae98d00e9
shefuchow <mschowdh@buffelo.edu>
2021-06-26 23:12:34 -0400

    added about-us page, added cards and dummy data

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Platform for Muslim Youth!
 HalalHacks Web App to connect young muslims to their community, and enable them with learning, growing, social and volunteering opportunities  
 
-Live at: 
+<!-- Need to run ng build --prod --baseHref="https://shefuchow.github.io/naltaqi/" to update deployment -->
+> [Web App now live, click here for demo](https://shefuchow.github.io/naltaqi/) 
 
 <!-- To Do: Update this link -->
 > [Devpost Submission](https://devpost.com/software/student-assistant) 
---
+++ frontend/src/app/about-us/about-us.component.ts
@@ -0,0 +1,145 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = [];
+  cardsForHandset = [
+    {
+      "imageName": "event",
+      "title": "Search events to go to and have fun",
+      "cols": 2,
+      "rows": 1
+  },
+  {
+      "imageName": "halaqa",
+      "title": "Meet for the sake of Allah to become 1% better muslims",
+      "cols": 2,
+      "rows": 1
+  },
+  {
+      "imageName": "youth",
+      "title": "Learn and socialize in Youth Programs at the masjid",
+      "cols": 2,
+      "rows": 1
+  },
+  {
+      "imageName": "volunteering",
+      "title": "Volunteer for the sake of Allah",
+      "cols": 2,
+      "rows": 1
+  }
+  ,
+  {
+      "imageName": "qurantutors",
+      "title": "Learn our holy quran from local quran teachers",
+      "cols": 2,
+      "rows": 1
+  },
+  {
+      "imageName": "muslimbusiness",
+      "title": "Shop at local muslim business to support the community!",
+      "cols": 2,
+      "rows": 1
+  }
+  ];
+  cardsForWeb = [
+    {
+      "imageName": "team",
+      "title": "Our team at Halal Hacks developed a platform for Muslim youth to connect to their community!",
+
+      "cols": 2,
+      "rows": 1
+  },
+  {
+      "imageName": "event",
+      "title": "Search events to go to and have fun",
+      "cols": 1,
+      "rows": 1
+  },
+  {
+      "imageName": "halaqa",
+      "title": "Meet for the sake of Allah to become 1% better muslims",
+      "cols": 1,
+      "rows": 1
+  },
+  {
+      "imageName": "youth",
+      "title": "Learn and socialize in Youth Programs at the masjid",
+      "cols": 1,
+      "rows": 1
+  },
+  {
+      "imageName": "volunteering",
+      "title": "Volunteer for the sake of Allah",
+      "cols": 1,
+      "rows": 1
+  }
+  ,
+  {
+      "imageName": "qurantutors",
+      "title": "Learn our holy quran from local quran teachers",
+      "cols": 1,
+      "rows": 1
+  },
+  {
+      "imageName": "muslimbusiness",
+      "title": "Shop at local muslim business to support the community!",
+      "cols": 1,
+      "rows": 1
+  }
+  ];
+
+  /*      E X A M P L E  C A R D  A R R A Y
+      [
+        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 1 },
+        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 4', cols: 1, rows: 1 }
+      ];
+  */
+
+  isHandset: boolean = false;
+  isHandsetObserver: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return true;
+      }
+      return false;
+    })
+  );
+
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isHandsetObserver.subscribe(currentObserverValue => {
+      console.log(currentObserverValue);
+    });
+  }
+
+  ngOnInit() {
+    this.isHandsetObserver.subscribe(currentObserverValue => {
+      this.isHandset = currentObserverValue;
+      this.loadCards();
+    });
+    console.log(this.isHandsetObserver);
+  }
+
+
+  loadCards() {
+    this.cards = this.isHandset ? this.cardsForHandset : this.cardsForWeb;
+  }
+
+  getImage(imageName: string): string {
+    //for DEV
+    return 'url(' + 'https://github.com/Shefuchow/naltaqi/blob/main/img/' + imageName + '.png?raw=true' + ')';
+    //For PROD
+    //Make sure to move copy of img folder into /Docs !!!!
+  }
+}
